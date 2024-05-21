a6ee897dd70a84f4699485351c9e1eabdc7cfeab
jinkshower <askthem@naver.com>
2024-05-21 15:25:44 +0900

    feat: StationGroup id와 Getter부여

--- src/main/java/com/hiyen/datacollection/stationgroup/domain/StationGroup.java
+++ src/main/java/com/hiyen/datacollection/stationgroup/domain/StationGroup.java
@@ -1,21 +1,35 @@
-package com.hiyen.datacollection.stationgroup;
+package com.hiyen.datacollection.stationgroup.domain;
 
 import com.hiyen.datacollection.exception.badrequest.SerialNumberInvalidException;
 import org.springframework.util.StringUtils;
 
 public class StationGroup {
 
+    private final Long id;
     private final String serialNumber;
 
-    public StationGroup(final String serialNumber) {
+    public StationGroup(final Long id, final String serialNumber) {
+        this.id = id;
         this.serialNumber = serialNumber;
 
         validateSerialNumber();
+    }
+
+    public StationGroup(final String serialNumber) {
+        this(null, serialNumber);
     }
 
     private void validateSerialNumber() {
         if (!StringUtils.hasText(this.serialNumber)) {
             throw new SerialNumberInvalidException(this.serialNumber);
         }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
     }
 }

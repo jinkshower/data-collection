6294159ed4d2a306658889f062160e4384e31f13
jinkshower <askthem@naver.com>
2024-05-22 15:12:20 +0900

    test: DeviceService 테스트 필요한 setup 추가

--- src/test/java/com/hiyen/datacollection/device/service/DeviceServiceTest.java
+++ src/test/java/com/hiyen/datacollection/device/service/DeviceServiceTest.java
@@ -1,9 +1,17 @@
 package com.hiyen.datacollection.device.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.hiyen.datacollection.common.mock.TestClockHolder;
+import com.hiyen.datacollection.common.service.port.ClockHolder;
 import com.hiyen.datacollection.device.controller.dto.request.DeviceRequest;
 import com.hiyen.datacollection.device.mock.FakeDeviceRepository;
+import com.hiyen.datacollection.device.service.port.DeviceRepository;
+import com.hiyen.datacollection.stationgroup.domain.StationGroup;
+import com.hiyen.datacollection.stationgroup.mock.FakeStationGroupRepository;
+import com.hiyen.datacollection.stationgroup.service.port.StationGroupRepository;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,10 +19,18 @@ import org.junit.jupiter.api.Test;
 class DeviceServiceTest {
 
     private DeviceService deviceService;
+    private LocalDateTime testNow;
 
     @BeforeEach
     void setup() {
-        deviceService = new DeviceService(new FakeDeviceRepository());
+        StationGroupRepository stationGroupRepository = new FakeStationGroupRepository();
+        DeviceRepository deviceRepository = new FakeDeviceRepository();
+        testNow = LocalDateTime.of(2013, 1, 1, 16, 0, 0);
+        ClockHolder clockHolder = new TestClockHolder(testNow);
+        deviceService = new DeviceService(deviceRepository, stationGroupRepository, clockHolder);
+
+        StationGroup stationGroup = new StationGroup("serial", testNow);
+        stationGroupRepository.save(stationGroup);
     }
 
     @DisplayName("Device를 생성할 수 있다")
@@ -30,8 +46,9 @@ class DeviceServiceTest {
 
         //then
         assertAll(
-            () -> assertNotNull(actual.getId()),
-            () -> assertEquals(serialNumber, actual.getSerialNumber())
+            () -> assertThat(actual.getSerialNumber()).isEqualTo(serialNumber),
+            () -> assertThat(actual.getStationGroupId()).isEqualTo(stationGroupId),
+            () -> assertThat(actual.getCreatedAt()).isEqualTo(testNow)
         );
     }
 }
